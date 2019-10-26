3de11377996e6a36029b28028630665ae8c14e67
Andrew Moss <andrew@m0ss.dev>
2019-10-26 16:01:08 -0600

    Starting on views

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MapBoard from '../components/mapBoard';
+
+function App() {
+  return (
+    <div className="App">
+		<div className="App">
+			<MapBoard/>
+		</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/mapContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// Presentation Component
+import Map from '../components/map';
+
+class MapContainer extends Component {
+
+	render() {
+		return (
+			React.createElement(Map, {})
+		)
+	}
+}
+
+export default MapContainer;
